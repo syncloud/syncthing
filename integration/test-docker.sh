72eb3b08a85b9d87447a0193d422b0875f1eac79
Boris Rybalkin <ribalkin@gmail.com>
2018-08-31 21:52:28 +0100

    fix archive

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -3,27 +3,24 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ "$#" -lt 6 ]; then
-    echo "usage $0 redirect_user redirect_password redirect_domain version release device_host"
+    echo "usage $0 redirect_user redirect_password redirect_domain release device_host archive"
     exit 1
 fi
 
 ARCH=$(uname -m)
-VERSION=$4
-RELEASE=$5
+RELEASE=$4
 DOMAIN=$3-${ARCH}-${DRONE_BRANCH}
-DEVICE_HOST=$6
+DEVICE_HOST=$5
+ARCHIVE=$6
 
 APP=syncthing
 
-if [ $ARCH == "x86_64" ]; then
+if [ ${ARCH} == "x86_64" ]; then
     TEST_SUITE="verify.py test-ui.py"
-    SNAP_ARCH=amd64
 else
     TEST_SUITE=verify.py
-    SNAP_ARCH=armhf
 fi
 
-ARCHIVE=${APP}_${VERSION}_${SNAP_ARCH}.snap
 APP_ARCHIVE_PATH=$(realpath "$ARCHIVE")
 
 cd ${DIR}
@@ -39,7 +36,7 @@ set +e
 sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} date
 while test $? -gt 0
 do
-  if [ $attempt -gt $attempts ]; then
+  if [ ${attempt} -gt ${attempts} ]; then
     exit 1
   fi
   sleep 3
@@ -49,7 +46,7 @@ do
 done
 set -e
 
-sshpass -p syncloud scp -o StrictHostKeyChecking=no install-${INSTALLER}.sh root@${DEVICE_HOST}:/installer.sh
+sshpass -p syncloud scp -o StrictHostKeyChecking=no install-snapd.sh root@${DEVICE_HOST}:/installer.sh
 
 sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} /installer.sh ${RELEASE}
 
@@ -59,7 +56,11 @@ pip2 install -r ${DIR}/dev_requirements.txt
 device_ip=$(getent hosts ${DEVICE_HOST} | awk '{ print $1 }')
 echo "$device_ip $DOMAIN.syncloud.info" >> /etc/hosts
 echo "$device_ip $APP.$DOMAIN.syncloud.info" >> /etc/hosts
+echo "$device_ip app.$DOMAIN.syncloud.info" >> /etc/hosts
 
 cat /etc/hosts
-
-xvfb-run -l --server-args="-screen 0, 1024x4096x24" py.test -x -s ${TEST_SUITE} --email=$1 --password=$2 --domain=$DOMAIN --release=$RELEASE --app-archive-path=${APP_ARCHIVE_PATH} --device-host=${DEVICE_HOST}
+py.test --fixtures
+xvfb-run -l --server-args="-screen 0, 1024x4096x24" py.test -x -s ${TEST_SUITE} \
+    --email=$1 --password=$2 --domain=${DOMAIN} --release=${RELEASE} \
+    --app-archive-path=${APP_ARCHIVE_PATH} --device-host=${DEVICE_HOST}
+    
