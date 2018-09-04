3d2f26166647cc59f8efb3e988449ef3e928c9b9
Boris Rybalkin <ribalkin@gmail.com>
2018-09-04 20:27:39 +0100

    try no exec in pre start

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -13,7 +13,7 @@ fi
 case $1 in
 pre-start)
     /bin/rm -f ${SNAP_COMMON}/web.socket
-    exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
+    ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     timeout 600 /bin/bash -c 'until echo > /dev/tcp/localhost/'$SYNCTHING_PORT'; do sleep 2; done'
     ;;
 start)
