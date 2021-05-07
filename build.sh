a12bd4af61525d1f07f1246b583a57521a95c9e5
Boris Rybalkin <ribalkin@gmail.com>
2021-05-07 17:52:41 +0100

    fix build

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ DOWNLOAD_URL=https://github.com/syncloud/3rdparty/releases/download/1
 
 ${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
 
-cd syncthing
+cd ${DIR}/build/syncthing
 go run build.go -version v${VERSION} tar
 tar xf syncthing-linux-*-v${VERSION}.tar.gz 
-mv syncthing-linux-*-v${VERSION} ${DIR}/build/syncthing 
+mv syncthing-linux-*-v${VERSION} syncthing 
 
 ${DIR}/build/syncthing/syncthing --help || true
