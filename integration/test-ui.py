430387a3604abe4922219e0b87dcf9ab7dc47cda
Boris Rybalkin <ribalkin@gmail.com>
2021-05-08 23:17:23 +0100

    try to fix screenshots

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -1,23 +1,11 @@
-import os
-import shutil
-from os.path import dirname, join, exists
 import time
-import pytest
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.support.ui import WebDriverWait
-from syncloudlib.integration.hosts import add_host_alias
-from syncloudlib.integration.screenshots import screenshots
+from os.path import dirname, join
 from subprocess import check_output
 
+import pytest
+from syncloudlib.integration.hosts import add_host_alias_by_ip
+
 DIR = dirname(__file__)
-screenshot_dir = join(DIR, 'screenshot')
 TMP_DIR = '/tmp/syncloud/ui'
 
 
@@ -34,15 +22,13 @@ def module_setup(request, device, artifact_dir, ui_mode):
     request.addfinalizer(module_teardown)
 
 
-def test_start(module_setup, app, device_host):
-    if not exists(screenshot_dir):
-        os.mkdir(screenshot_dir)
-
-    add_host_alias(app, device_host)
+def test_start(module_setup, app, domain, device_host):
+    add_host_alias_by_ip(app, domain, device_host)
 
 
-def test_login(driver, app_domain, ui_mode, device_user, device_password):
+def test_login(driver, app_domain, selenium, device_user, device_password):
 
     driver.get("https://{0}:{1}@{2}".format(device_user, device_password, app_domain))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'login-' + ui_mode)
+    selenium.screenshot('login')
+
