2a2deedb94fa0ece3569da3c4f98a6d8ae490795
Boris Rybalkin <ribalkin@gmail.com>
2021-05-08 23:02:29 +0100

    cleanup

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -14,23 +14,24 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.support.ui import WebDriverWait
 from syncloudlib.integration.hosts import add_host_alias
 from syncloudlib.integration.screenshots import screenshots
+from subprocess import check_output
 
 DIR = dirname(__file__)
 screenshot_dir = join(DIR, 'screenshot')
 TMP_DIR = '/tmp/syncloud/ui'
 
+
 @pytest.fixture(scope="session")
-def module_setup(request, device, log_dir, ui_mode):
-    request.addfinalizer(lambda: module_teardown(device, log_dir, ui_mode))
+def module_setup(request, device, artifact_dir, ui_mode):
+    def module_teardown():
+        device.activated()
+        device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
+        device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
+        device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
+        device.scp_from_device('{0}/*'.format(TMP_DIR), join(artifact_dir, 'log'))
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
 
-
-def module_teardown(device, log_dir, ui_mode):
-    device.activated()
-    device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
-    device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
-    device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
-      
-    device.scp_from_device('{0}/*'.format(TMP_DIR), join(log_dir, 'log'))
+    request.addfinalizer(module_teardown)
 
 
 def test_start(module_setup, app, device_host):
