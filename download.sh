d48405e56a56e6ca68fc8b604617769dfb8a58a0
Boris Rybalkin <ribalkin@gmail.com>
2022-02-04 23:21:46 +0000

    fix download

--- download.sh
+++ download.sh
@@ -4,16 +4,15 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 VERSION=$1
 ARCH=$(uname -m)
-BUILD_DIR=${DIR}/build/app
 
 apt update
 apt -y install wget
 
-cd $BUILD_DIR
-
+cd ${DIR}/build/app
 wget -c --progress=dot:giga https://github.com/syncloud/3rdparty/releases/download/nginx/nginx-${ARCH}.tar.gz
 tar xf nginx-${ARCH}.tar.gz
 
+cd ${DIR}/build
 wget https://github.com/syncthing/syncthing/archive/v${VERSION}.tar.gz
 tar xf v${VERSION}.tar.gz
 mv syncthing-${VERSION} syncthing-src
