d04cb490d28efe5ce6e28461a01a6f474f9635eb
Boris Rybalkin <ribalkin@gmail.com>
2022-11-12 09:24:05 +0000

    password warning test

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -4,6 +4,7 @@ from subprocess import check_output
 
 import pytest
 from syncloudlib.integration.hosts import add_host_alias
+from selenium.webdriver.common.by import By
 
 DIR = dirname(__file__)
 TMP_DIR = '/tmp/syncloud/ui'
@@ -29,9 +30,12 @@ def test_start(module_setup, app, device, device_host, domain):
 def test_login(selenium, app_domain, device_user, device_password):
     selenium.driver.get("https://{0}:{1}@{2}".format(device_user, device_password, app_domain))
 
-    #selenium.open_app()
-    time.sleep(10)
+    selenium.find_by_xpath("//h3[text()='This Device']")
+    timw.sleep(20)
     selenium.screenshot('index')
+    passwordWarnings = selenium.driver.find_elements(By.XPATH, "//span[text()='GUI Authentication: Set User and Password']")
+    assert len(passwordWarnings) == 0
+    
 
 
 def test_teardown(driver):
