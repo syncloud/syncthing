be6fedc7d95cc9aaf52d445df21d3f7ed472e70f
Boris Rybalkin <ribalkin@gmail.com>
2024-02-24 09:53:09 +0000

    fix socket cleanup

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 . /var/snap/syncthing/current/config/syncthing.env
 
-/bin/rm -f ${SNAP_COMMON}/common/web.socket
+/bin/rm -f ${SNAP_COMMON}/web.socket
 /bin/rm -f ${SNAP_COMMON}/log/nginx*.log
 timeout 60 /bin/bash -c 'until echo > /dev/tcp/localhost/'$SYNCTHING_PORT'; do sleep 10; done'
 exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -e stderr
