490db98717bac743c713a154c5dbda57579c591c
Boris Rybalkin <ribalkin@gmail.com>
2026-09-12 11:12:49 +0100

    Drop the unused start/stop dispatch from the syncthing launcher
    The script only ever ran as 'bin/service.syncthing.sh start' from snap.yaml,
    and there was no stop branch behind the usage string that advertised one -
    systemd stops the daemon with a signal, it does not re-invoke the launcher.
    No sibling app passes an argument to a service script.
    
    Also takes the home path from $SNAP_DATA rather than hardcoding it, matching
    service.nginx.sh.

--- bin/service.syncthing.sh
+++ bin/service.syncthing.sh
@@ -2,18 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-if [[ -z "$1" ]]; then
-    echo "usage $0 [start|stop]"
-    exit 1
-fi
+sysctl -w fs.inotify.max_user_watches=204800
 
-case $1 in
-start)
-    sysctl -w fs.inotify.max_user_watches=204800
-    exec ${DIR}/syncthing --home /var/snap/syncthing/current/config/syncthing serve
-    ;;
-*)
-    echo "not valid command"
-    exit 1
-    ;;
-esac
+exec ${DIR}/syncthing --home ${SNAP_DATA}/config/syncthing serve
